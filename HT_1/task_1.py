499ee347c4f4654f8b6bb8388c709cb09b8f263b
Oleksandr_Halushka <alexandergalushka@gmail.com>
2021-11-16 21:23:17 +0200

    task1 update

--- HT_1/task_1.py
+++ HT_1/task_1.py
@@ -1,6 +1,8 @@
-if __name__ == "__main__":
-    sequence = input()
+def list_tuple_maker(sequence):
     sequence_list = sequence.split(',')
     sequence_tuple = tuple(sequence.split(','))
     print(f' List : {sequence_list} \n Tuple : {sequence_tuple}')
 
+
+if __name__ == "__main__":
+    list_tuple_maker(input('Input your sequence here :'))
