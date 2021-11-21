966cdaf85f1bce5868258057b0e4fbf735f7babc
Oleksandr_Halushka <alexandergalushka@gmail.com>
2021-11-21 12:23:25 +0200

    task 5 updated

--- HT_2/task5.py
+++ HT_2/task5.py
@@ -11,18 +11,16 @@
 def dictionary_cleaner(some_dictionary):
     cleaned_dictionary = {}
     our_keys = some_dictionary.keys()
-    values = []
     for index in our_keys:
-        if some_dictionary[index] in values:
+        if some_dictionary[index] in cleaned_dictionary.values():
             continue
         else:
-            values.append(some_dictionary[index])
             cleaned_dictionary[index] = some_dictionary[index]
     return cleaned_dictionary
 
 
 if __name__ == '__main__':
-    strange_dictionary = {'a': 1, 'b': 3, 'c': 6, 'd': 8, 'e': 9, 'f': 11, 'g': 3, 'h': 4, 'i': 5, 'j': 4, 'k': 2,
-                          'l': 3, 'm': 1, 'n': 8, 'o': 5, 'p': 9, 'q': 4, 'r': 4, 's': 6, 't': 4, 'y': 1}
+    strange_dictionary = {'a': 1, 'b': 3, 'c': [6, 7], 'd': 8, 'e': 9, 'f': 11, 'g': 3, 'h': 4, 'i': 5, 'j': 4, 'k': 2,
+                          'l': 3, 'm': 1, 'n': 8, 'o': [6, 7], 'p': 9, 'q': 4, 'r': 4, 's': 6, 't': 4, 'y': 1}
 
     print(dictionary_cleaner(strange_dictionary))
