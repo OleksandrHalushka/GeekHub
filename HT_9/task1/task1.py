50e5bc6708579aa4adc3222bd691558e41521c17
Oleksandr_Halushka <alexandergalushka@gmail.com>
2021-12-18 23:46:06 +0200

    HT_10 complete

--- HT_9/task1/task1.py
+++ HT_9/task1/task1.py
@@ -16,12 +16,12 @@ import datetime
 import json
 from collections import Counter
 
-conn = sqlite3.connect('atm.db')
+conn = sqlite3.connect('../../HT_10/task1/atm.db')
 cur = conn.cursor()
 
 
 class User:
-    conn = sqlite3.connect('atm.db')
+    conn = sqlite3.connect('../../HT_10/task1/atm.db')
     cur = conn.cursor()
 
     def __init__(self, username):
@@ -63,7 +63,7 @@ class User:
 
 
 class Atm:
-    conn = sqlite3.connect('atm.db')
+    conn = sqlite3.connect('../../HT_10/task1/atm.db')
     cur = conn.cursor()
 
     def __init__(self):
@@ -92,10 +92,10 @@ class NegativeMeaning(Exception):
 
 
 def create_db():
-    conn = sqlite3.connect('atm.db')
+    conn = sqlite3.connect('../../HT_10/task1/atm.db')
     cur = conn.cursor()
     if input('Would you like to create database? (yes/no)') == 'yes':
-        fop = open('atm.db', 'w')
+        fop = open('../../HT_10/task1/atm.db', 'w')
         fop.close()
         cur.execute("""CREATE TABLE IF NOT EXISTS users(
                user_id INT PRIMARY KEY,
@@ -200,7 +200,7 @@ def unblocker(login):
 
 
 def new_user(login):
-    conn = sqlite3.connect('atm.db')
+    conn = sqlite3.connect('../../HT_10/task1/atm.db')
     cur = conn.cursor()
     users = cur.execute(f"SELECT username FROM users").fetchall()
     for user in users:
@@ -386,6 +386,7 @@ def collectors_menu(login):
 
 
 def start():
+
     if input('Do you have an account? (print yes or no) ') == 'yes':
         login = input('Hello, please input your login: ')
         try:
