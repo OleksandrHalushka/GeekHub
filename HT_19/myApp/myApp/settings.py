07e82ed0a2a1d99624b8976f91e0489e2aed5007
Oleksandr_Halushka <alexandergalushka@gmail.com>
2022-02-04 16:26:55 +0200

    HT19_updated

--- HT_19/myApp/myApp/settings.py
+++ HT_19/myApp/myApp/settings.py
@@ -75,7 +75,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'myApp.wsgi.application'
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
